5bc890dba86f95abc055817063aca00ba4a7fc56
SahanDilusha <sahancodelygroup@gmail.com>
2024-12-03 17:24:02 +0530

    user login done

--- src/java/controller/Login.java
+++ src/java/controller/Login.java
@@ -6,6 +6,7 @@ import com.google.gson.JsonObject;
 import entity.User;
 import entity.UserStatus;
 import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,54 +17,47 @@ import model.Validations;
 import org.hibernate.Session;
 import org.hibernate.criterion.Restrictions;
 
-@WebServlet(name = "SignUp", urlPatterns = {"/SignUp"})
-public class SignUp extends HttpServlet {
-
+@WebServlet(name = "Login", urlPatterns = {"/Login"})
+public class Login extends HttpServlet {
+    
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        
         Session session = HibernateUtil.getSessionFactory().openSession();
         Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
         JsonObject responseObject = new JsonObject();
-
+        
         try {
-
+            
             JsonObject reuestObject = (JsonObject) gson.fromJson(req.getReader(), JsonObject.class);
-
+            
             responseObject.addProperty("status", false);
-
+            
             if (reuestObject == null) {
                 responseObject.addProperty("content", "Request Data missing!");
-            } else if (reuestObject.get("name").getAsString().isEmpty()) {
-                responseObject.addProperty("content", "Please enter your name!");
             } else if (!Validations.isEmailValid(reuestObject.get("email").getAsString())) {
                 responseObject.addProperty("content", "Invalide Email Addres!");
             } else if (!Validations.isPasswordValid(reuestObject.get("password").getAsString())) {
                 responseObject.addProperty("content", "Invalide Password!");
-            } else if (!reuestObject.get("confirmPassword").getAsString().equals(reuestObject.get("password").getAsString())) {
-                responseObject.addProperty("content", "Confirm Your Password!");
-            } else if (session.createCriteria(User.class).add(Restrictions.eq("email", reuestObject.get("email").getAsString())).uniqueResult() == null) {
-
-                UserStatus userStatus = (UserStatus) session.get(UserStatus.class, 1);
-
-                User newUser = new User(reuestObject.get("name").getAsString(), reuestObject.get("email").getAsString(), "123456", reuestObject.get("password").getAsString(), userStatus);
-
-                session.save(newUser);
-
-                session.beginTransaction().commit();
-
-                responseObject.addProperty("status", true);
-                responseObject.add("content", gson.toJsonTree(newUser));
             } else {
-                responseObject.add("content", gson.toJsonTree("This email is already registered."));
+                
+                User user = (User) session.createCriteria(User.class).add(Restrictions.eq("email", reuestObject.get("email").getAsString())).add(Restrictions.eq("password", reuestObject.get("password").getAsString())).uniqueResult();
+                
+                if (user != null) {
+                    responseObject.addProperty("status", true);
+                    responseObject.add("content", gson.toJsonTree(user));
+                } else {
+                    responseObject.addProperty("content", "Inavalide email or password!");
+                }
+                
             }
-
+            
             resp.setContentType("application/json");
             resp.getWriter().write(gson.toJson(responseObject));
-
+            
         } catch (Exception e) {
             e.printStackTrace();
-            responseObject.add("content", gson.toJsonTree("System error!"));
+            responseObject.addProperty("content", "System error!");
             resp.setContentType("application/json");
             resp.getWriter().write(gson.toJson(responseObject));
         } finally {
@@ -71,7 +65,7 @@ public class SignUp extends HttpServlet {
                 session.close();
             }
         }
-
+        
     }
-
+    
 }
